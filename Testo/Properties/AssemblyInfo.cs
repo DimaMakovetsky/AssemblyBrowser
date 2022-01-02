fefc77943030b48bcd238771e9544737ed61787b
DimaMakovetsky <mister.chuppa@mail.ru>
2022-01-02 16:53:39 +0300

    Testo

---
+++ Testo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff23c9e5-a6b8-480f-84ec-8df9b0a2287e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testo/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyChecker;
+using System.Collections.Generic;
+
+namespace Testo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string _testAssemblyPath = "/probably/Cringe.dll";
+        private AssemblyCheck _assemblyBrowser;
+        private List<ContainerInAssembly> _assemblyInfo;
+
+        [TestMethod]
+        public void AssemblyIsNotEmptyTest()
+        {
+            _assemblyBrowser = new AssemblyCheck();
+            _assemblyInfo = _assemblyBrowser.GetAssemblyInfo(AppDomain.CurrentDomain.BaseDirectory + _testAssemblyPath);
+
+            int expected = 0;
+            int actual = _assemblyInfo.Count;
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AssemblyHasCorrectClassesNumberTest()
+        {
+
+            _assemblyBrowser = new AssemblyCheck();
+            _assemblyInfo = _assemblyBrowser.GetAssemblyInfo(AppDomain.CurrentDomain.BaseDirectory + _testAssemblyPath);
+            int expected = 3;
+            int actual = _assemblyInfo[0].Members.Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AssemblyClassHasCorrectMembersNumberTest()
+        {
+
+            _assemblyBrowser = new AssemblyCheck();
+            _assemblyInfo = _assemblyBrowser.GetAssemblyInfo(AppDomain.CurrentDomain.BaseDirectory + _testAssemblyPath);
+            int expected = 3;
+            ContainerInAssembly container = (ContainerInAssembly)_assemblyInfo[0].Members[0];
+            int actual = container.Members.Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IncorrectAssemblyPathTest()
+        {
+
+            _assemblyBrowser = new AssemblyCheck();
+            _assemblyInfo = _assemblyBrowser.GetAssemblyInfo(AppDomain.CurrentDomain.BaseDirectory + _testAssemblyPath);
+            string path = "../qwe//Assembly.dll";
+            Assert.ThrowsException<FileNotFoundException>(() => _assemblyBrowser.GetAssemblyInfo(path));
+        }
+    }
+}
